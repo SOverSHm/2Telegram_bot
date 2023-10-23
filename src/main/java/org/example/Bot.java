d7b10cf4a6686e59b3be18599109bc769d1cf398
Name <s-shmorgun0.01@yandex.ru>
2023-10-23 23:18:28 +0300

    Initial commit

---
+++ src/main/java/org/example/Bot.java
@@ -0,0 +1,109 @@
+package org.example;
+
+import org.example.functions.FilterOperatons;
+import org.example.utils.ImageUtils;
+import org.example.utils.PhotoMassageUtils;
+import org.example.utils.RgbMaster;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.GetFile;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+
+        return "another_my_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+
+        return "6690554949:AAH9kWTfDGjqt1OReaKWPKmStkxPVpJPzqc";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        String chatId = message.getChatId().toString();
+        try {
+            ArrayList<String> photoPaths = new ArrayList<>(PhotoMassageUtils.savePhotos(getFilesByMessage(message), getBotToken()));
+            for (String path : photoPaths) {
+                processingImage(path);
+                execute(preparePhotoMessage(path, chatId));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void processingImage(String fileName) throws Exception {
+        final BufferedImage image = ImageUtils.getImage(fileName);
+        final RgbMaster rgbMaster = new RgbMaster(image);
+        rgbMaster.changeImage(FilterOperatons::grayScale);
+        ImageUtils.saveImage(rgbMaster.getImage(), fileName);
+    }
+
+    private SendPhoto preparePhotoMessage(String localPath, String chatId) {
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setReplyMarkup(getKeyboard(FilterOperatons.class));
+        sendPhoto.setChatId(chatId);
+        InputFile newFile = new InputFile();
+        newFile.setMedia(new File(localPath));
+        sendPhoto.setPhoto(newFile);
+        return sendPhoto;
+    }
+
+    private List<org.telegram.telegrambots.meta.api.objects.File> getFilesByMessage(Message message) {
+        List<PhotoSize> photoSizes = message.getPhoto();
+        if (photoSizes == null) return new ArrayList<>();
+        ArrayList<org.telegram.telegrambots.meta.api.objects.File> files = new ArrayList<>();
+        for (PhotoSize photoSize : photoSizes) {
+            final String fileId = photoSize.getFileId();
+            try {
+                files.add(sendApiMethod(new GetFile(fileId)));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+        return files;
+    }
+
+    private ReplyKeyboardMarkup getKeyboard(Class someClass) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        Method[] methods = someClass.getMethods();
+        int columnCount = 3;
+        int rowsCount = methods.length / columnCount + ((methods.length % columnCount == 0) ? 0 : 1);
+        for (int rowIndex = 0; rowIndex < rowsCount; rowIndex++) {
+            KeyboardRow row = new KeyboardRow();
+            for (int columIndex =0; columIndex < columnCount; columIndex++) {
+                int index = rowIndex * columnCount + columIndex;
+                if (index >= methods.length) continue;
+                Method method = methods[rowIndex * columnCount + columIndex];
+                KeyboardButton keyboardButton = new KeyboardButton(method.getName());
+                KeyboardButton KeyboardButton;
+                row.add(keyboardButton);
+            }
+            keyboardRows.add(row);
+        }
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+        return replyKeyboardMarkup;
+    }
+}
